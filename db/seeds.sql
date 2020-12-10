0aa571f1256108853c92cba66cb4d708c98b9d7e
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-12-09 22:27:12 -0800

    first commit with starter code

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Western BBQ", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pepperjack Jalapeno", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Classic American", false);
