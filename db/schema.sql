f0e16f48f3d77364c4caa3dc1d3bff4da2408849
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-12-13 11:29:15 -0800

    typos :(

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-drop table burgers;
-CREATE TABLE burgers (
+  CREATE DATABASE burgers_db;
+  USE burgers_db;
+  drop table burgers;
+  CREATE TABLE burgers (
 
-  id INT NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(255),
-  devoured BOOLEAN,
-  PRIMARY KEY (id)
-);
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255),
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+  );
